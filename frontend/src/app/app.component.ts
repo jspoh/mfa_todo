275210f18f5baaf6762a8e427980a53e3201710d
jspoh <jingsengyeah@gmail.com>
2023-06-23 21:15:15 +0800

    fix for previous commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -18,9 +18,15 @@ export class AppComponent {
       .subscribe({
         next: (val) => {
           // console.log(val);
-          userService.userData.userId.next(val.userId);
-          userService.userData.username.next(val.username);
-          userService.userData.name.next(val.name);
+          if (val.loggedIn) {
+            userService.userData.userId.next(val.data.userId);
+            userService.userData.username.next(val.data.username);
+            userService.userData.name.next(val.data.name);
+            console.log(`Logged in as ${val.data.username}`);
+          } else {
+            console.log('Guest user');
+          }
+
         },
         error: (err) => {
           console.error(err);
