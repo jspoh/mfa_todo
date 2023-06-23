e481848cca7297439c83f0c0fcd5c262eca72db1
jspoh <jingsengyeah@gmail.com>
2023-06-23 21:35:05 +0800

    init light frontend security

--- frontend/src/app/features/todos/todos.component.ts
+++ frontend/src/app/features/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,11 +9,18 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  viewingUser = '';
+  viewingPermissions = false;  // light frontend security. need to implement server side too => frontend easy to hack
+  
+  todos = [];
 
-  constructor(public userService: UserService) { }
+  constructor(private userService: UserService, router: Router, dataService: DataService) { 
+    this.viewingUser = router.url.replace('/', '');
+    this.viewingPermissions = (userService.userData.username$.getValue() == this.viewingUser);
+  }
 
   ngOnInit(): void {
-
+    
   }
 
 }
